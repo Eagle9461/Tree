74e4fd533d835f1cad0cd11ba77a7b67126aaac3
DESKTOP-9NC8JV4\K <eagle9461@outlook.com>
2024-05-17 02:00:28 +0200

    init

---
+++ src/config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  // proctree
+  seed: 256,
+  segments: 6,
+  levels: 0.1,
+  vMultiplier: 2.36,
+  twigScale: 0.3,
+  initalBranchLength: 0.45,
+  lengthFalloffFactor: 0.85,
+  lengthFalloffPower: 0.99,
+  clumpMax: 0.9,
+  clumpMin: 0.404,
+  branchFactor: 2.45,
+  dropAmount: -0.1,
+  growAmount: 0.235,
+  sweepAmount: 0.01,
+  maxRadius: 0.05,
+  climbRate: 0.05,
+  trunkKink: 0.093,
+  treeSteps: 0,
+  taperRate: 0.947,
+  radiusFalloffRate: 0.5,
+  twistRate: 3.02,
+  trunkLength: 0.1,
+
+  // custom
+  treeColor: 0x9d7362,
+  twigColor: 0xF16950
+};
