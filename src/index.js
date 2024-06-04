b27a1eec3e1a3d2101eddad48d3d7c7867f6526c
DESKTOP-9NC8JV4\K <eagle9461@outlook.com>
2024-06-04 10:40:00 -0500

    Add Grass Feature

--- src/index.js
+++ src/index.js
@@ -8,9 +8,68 @@ const DEFAULT_CONFIG = require('./config');
 const Viewer = require('./viewer');
 const download = require('downloadjs');
 
+const vert = `
+
+varying vec2 vUv;
+varying vec2 cloudUV;
+varying vec3 vColor;
+uniform float iTime;
+
+void main() {
+    vUv = uv;
+    cloudUV = uv;
+    vColor = color;
+    vec3 cpos = position;
+
+    float waveSize = 10.0;
+    float tipDistance = 0.3;
+    float centerDistance = 0.1;
+
+    if (color.x > 0.6) {
+        cpos.x += sin((iTime / 500.0) + (uv.x * waveSize)) * tipDistance;
+    } else if (color.x > 0.0) {
+        cpos.x += sin((iTime / 500.0) + (uv.x * waveSize)) * centerDistance;
+    }
+
+    float diff = position.x - cpos.x;
+    cloudUV.x += iTime / 20000.0;
+    cloudUV.y += iTime / 10000.0;
+
+    vec4 worldPosition = vec4(cpos, 1.0);
+    vec4 mvPosition = projectionMatrix * modelViewMatrix * vec4(cpos, 1.0);
+    gl_Position = mvPosition;
+}
+`;
+
+const frag = `
+uniform sampler2D texture1;
+uniform sampler2D textures[4];
+
+varying vec2 vUv;
+varying vec2 cloudUV;
+varying vec3 vColor;
+
+void main() {
+    float contrast = 1.5;
+    float brightness = 0.1;
+    vec3 texColor = texture2D(textures[0], vUv).rgb * contrast;
+    texColor += vec3(brightness, brightness, brightness);
+    texColor = mix(texColor, texture2D(textures[1], cloudUV).rgb, 0.4);
+    gl_FragColor.rgb = texColor;
+    gl_FragColor.a = 1.0;
+}
+`;
+
 let mtlLoader = new MTLLoader();
  
 let objLoader = new OBJLoader();
+
+// Parameters
+const PLANE_SIZE = 50;
+const BLADE_COUNT = 300000;
+const BLADE_WIDTH = 0.05;
+const BLADE_HEIGHT = 0.5;
+const BLADE_HEIGHT_VARIATION = 0.4;
 
 class App {
   constructor(el) {
@@ -37,10 +96,125 @@ class App {
     this.currentMaxRadius = this.config.maxRadius;
     this.targetMaxRadius = this.config.targetMaxRadius;
 
+    // Grass Texture
+    this.grassTexture = new THREE.TextureLoader().load('assets/grass.jpg');
+    this.cloudTexture = new THREE.TextureLoader().load('assets/cloud.jpg');
+    this.cloudTexture.wrapS = this.cloudTexture.wrapT = THREE.RepeatWrapping;
+
+    this.timeUniform = { type: 'f', value: 0.0 };
+    this.grassUniforms = {
+      textures: { value: [this.grassTexture, this.cloudTexture] },
+      iTime: this.timeUniform
+    };
+
+    this.grassMaterial = new THREE.ShaderMaterial({
+      uniforms: this.grassUniforms,
+      vertexShader: vert,
+      fragmentShader: frag,
+      vertexColors: true,
+      side: THREE.DoubleSide
+    });
+
     this.addGUI();
     this.initGround();
     this.plantSeed();
   }
+
+  convertRange (val, oldMin, oldMax, newMin, newMax) {
+    return (((val - oldMin) * (newMax - newMin)) / (oldMax - oldMin)) + newMin;
+  }
+  
+  generateField () {
+    const positions = [];
+    const uvs = [];
+    const indices = [];
+    const colors = [];
+
+    for (let i = 0; i < BLADE_COUNT; i++) {
+      const VERTEX_COUNT = 5;
+      const surfaceMin = PLANE_SIZE / 2 * -1;
+      const surfaceMax = PLANE_SIZE / 2;
+      const radius = PLANE_SIZE / 2;
+
+      const r = radius * Math.sqrt(Math.random());
+      const theta = Math.random() * 2 * Math.PI;
+      const x = r * Math.cos(theta);
+      const y = r * Math.sin(theta);
+
+      const pos = new THREE.Vector3(x, -3, y);
+
+      const uv = [this.convertRange(pos.x, surfaceMin, surfaceMax, 0, 1), this.convertRange(pos.z, surfaceMin, surfaceMax, 0, 1)];
+
+      const blade = this.generateBlade(pos, i * VERTEX_COUNT, uv);
+      blade.verts.forEach(vert => {
+        positions.push(...vert.pos);
+        uvs.push(...vert.uv);
+        colors.push(...vert.color);
+      });
+      blade.indices.forEach(indice => indices.push(indice));
+    }
+
+    const geom = new THREE.BufferGeometry();
+    geom.addAttribute('position', new THREE.BufferAttribute(new Float32Array(positions), 3));
+    geom.addAttribute('uv', new THREE.BufferAttribute(new Float32Array(uvs), 2));
+    geom.addAttribute('color', new THREE.BufferAttribute(new Float32Array(colors), 3));
+    geom.setIndex(indices);
+    geom.computeVertexNormals();
+    geom.computeFaceNormals();
+
+    const mesh = new THREE.Mesh(geom, this.grassMaterial);
+    this.viewer.scene.add(mesh);
+  }
+
+  generateBlade (center, vArrOffset, uv) {
+    const MID_WIDTH = BLADE_WIDTH * 0.5;
+    const TIP_OFFSET = 0.1;
+    const height = BLADE_HEIGHT + (Math.random() * BLADE_HEIGHT_VARIATION);
+
+    const yaw = Math.random() * Math.PI * 2;
+    const yawUnitVec = new THREE.Vector3(Math.sin(yaw), 0, -Math.cos(yaw));
+    const tipBend = Math.random() * Math.PI * 2;
+    const tipBendUnitVec = new THREE.Vector3(Math.sin(tipBend), 0, -Math.cos(tipBend));
+
+    // Find the Bottom Left, Bottom Right, Top Left, Top right, Top Center vertex positions
+    const bl = new THREE.Vector3().addVectors(center, new THREE.Vector3().copy(yawUnitVec).multiplyScalar((BLADE_WIDTH / 2) * 1));
+    const br = new THREE.Vector3().addVectors(center, new THREE.Vector3().copy(yawUnitVec).multiplyScalar((BLADE_WIDTH / 2) * -1));
+    const tl = new THREE.Vector3().addVectors(center, new THREE.Vector3().copy(yawUnitVec).multiplyScalar((MID_WIDTH / 2) * 1));
+    const tr = new THREE.Vector3().addVectors(center, new THREE.Vector3().copy(yawUnitVec).multiplyScalar((MID_WIDTH / 2) * -1));
+    const tc = new THREE.Vector3().addVectors(center, new THREE.Vector3().copy(tipBendUnitVec).multiplyScalar(TIP_OFFSET));
+
+    tl.y += height / 2;
+    tr.y += height / 2;
+    tc.y += height;
+
+    // Vertex Colors
+    const black = [0, 0, 0];
+    const gray = [0.5, 0.5, 0.5];
+    const white = [1.0, 1.0, 1.0];
+
+    const verts = [
+      { pos: bl.toArray(), uv: uv, color: black },
+      { pos: br.toArray(), uv: uv, color: black },
+      { pos: tr.toArray(), uv: uv, color: gray },
+      { pos: tl.toArray(), uv: uv, color: gray },
+      { pos: tc.toArray(), uv: uv, color: white }
+    ];
+
+    const indices = [
+      vArrOffset,
+      vArrOffset + 1,
+      vArrOffset + 2,
+      vArrOffset + 2,
+      vArrOffset + 4,
+      vArrOffset + 3,
+      vArrOffset + 3,
+      vArrOffset,
+      vArrOffset + 2
+    ];
+
+    return { verts, indices };
+  }
+
 
   plantSeed() {
     const fbxLoader = new FBXLoader();
@@ -62,7 +236,7 @@ class App {
   }
   
   initGround() {
-    const geometry = new THREE.PlaneGeometry(50, 50); // You can adjust the size as needed
+    const geometry = new THREE.PlaneGeometry(35, 35); // You can adjust the size as needed
     const material = new THREE.MeshStandardMaterial({
       color: 0x006600, // A dark green, resembling grass
       roughness: 2,
@@ -70,7 +244,7 @@ class App {
     });
 
     const textureLoader = new THREE.TextureLoader();
-    material.map = textureLoader.load('../assets/ground.jpeg');
+    material.map = textureLoader.load('../assets/grass.jpg');
     // material.bumpScale = 2;
     // material.displacementScale = 0.2;
 
@@ -80,6 +254,8 @@ class App {
     this.ground.receiveShadow = true;
 
     this.viewer.scene.add(this.ground);
+
+    this.generateField();
   }
 
   initWaterParticles() {
---
+++ src/shaders/grass.js
@@ -0,0 +1,3 @@
+const vert = require('./glsl/grass.vert.glsl');
+const frag = require('./glsl/grass.frag.glsl');
+export default { frag, vert };
